caa57a1433736995ff718bd323fe4fe454f3e1f3
VladGershun <VladYGershun@Yahoo.com>
2021-12-11 20:55:23 -0800

    Changed code to use computed properties.

--- Day6/Day6_Practice.playground/Contents.swift
+++ Day6/Day6_Practice.playground/Contents.swift
@@ -5,40 +5,39 @@ enum Family {
     case mom
     case sister
     case brother
-}
-
-var currentPerson = Family.dad
-
-var name: String {
-    switch currentPerson {
-    case .dad:
-        return "Yuriy Gershun"
-    case .mom:
-        return "Yelena Gershun"
-    case .sister:
-        return "Esther Gershun"
-    case .brother:
-        return "Vlad Gershun"
+    
+    var name: String {
+        switch self {
+        case .dad:
+            return "Yuriy Gershun"
+        case .mom:
+            return "Yelena Gershun"
+        case .sister:
+            return "Esther Gershun"
+        case .brother:
+            return "Vlad Gershun"
+        }
+    }
+    
+    var age: Int {
+        switch self {
+        case .dad:
+            return 57
+        case .mom:
+            return 46
+        case .sister:
+            return 22
+        case .brother:
+            return 24
+        }
+    }
+    
+    var info: (String, Int) {
+        return (self.name, self.age)
     }
 }
 
-var age: Int {
-    switch currentPerson {
-    case .dad:
-        return 57
-    case .mom:
-        return 46
-    case .sister:
-        return 22
-    case .brother:
-        return 24
-    }
-}
+print(Family.dad.info)
+print(Family.mom.info)
 
-func info(person: Family) -> (String, Int) {
-    currentPerson = person
-    return (name, age)
-}
-
-print(info(person: Family.brother))
 
