4b9cb8d2d4d45e2c46747c8ec1213ea2b13273b8
VladGershun <VladYGershun@Yahoo.com>
2021-12-18 04:12:03 -0800

    Created empty project for WeSplit.

---
+++ Project1/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Vlad Gershun on 12/18/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
