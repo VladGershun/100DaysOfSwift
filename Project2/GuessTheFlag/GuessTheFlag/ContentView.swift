9c84eedd33dea9df22b9db7fe63dad8885d2a768
VladGershun <VladYGershun@Yahoo.com>
2021-12-20 21:05:28 -0800

    Day 22 completed. Added game over functionality.

--- Project2/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ Project2/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -10,14 +10,15 @@ import SwiftUI
 struct ContentView: View {
     @State private var showingScore = false
     @State private var scoreTitle = ""
+    @State private var currentScore = 0
+    @State private var gameInfo = "You Won!"
+    @State private var gameOver = false
     
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
     
     var body: some View {
         ZStack {
-//            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
-//                .ignoresSafeArea()
             RadialGradient(stops: [
                 .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
                 .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
@@ -59,28 +60,34 @@ struct ContentView: View {
                 Spacer()
                 Spacer()
                 
-                Text("Scoore: ???")
+                Text("Score: \(currentScore)")
                     .foregroundColor(.white)
                     .font(.title.bold())
                 
                 Spacer()
             }
             .padding()
-                
-
+            
+            
         }
-         .alert(scoreTitle, isPresented: $showingScore) {
+        .alert(scoreTitle, isPresented: $showingScore) {
             Button("Continue", action: askQuestion)
         } message: {
-            Text("Your score is ???")
+            Text("Your score is \(currentScore)")
+        }
+        .alert(gameInfo, isPresented: $gameOver) {
+            Button("New Game", action: resetGame)
+        } message: {
+            Text("Your score is \(currentScore)")
         }
     }
     
     func flagTapped(_ number: Int) {
         if number == correctAnswer {
             scoreTitle = "Correct"
+            currentScore += 1
         } else {
-            scoreTitle = "Wrong"
+            scoreTitle = "Wrong, you chose \(countries[number])"
         }
         
         showingScore = true
@@ -89,6 +96,14 @@ struct ContentView: View {
     func askQuestion() {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
+        resetGame()
+    }
+    
+    func resetGame() {
+        if currentScore == 8 {
+            currentScore = 0
+            gameOver = true
+        }
     }
     
 }
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 Course Link: www.hackingwithswift.com/100/swiftui
 
-Current Day Completed: 21
+Current Day Completed: 22
 
