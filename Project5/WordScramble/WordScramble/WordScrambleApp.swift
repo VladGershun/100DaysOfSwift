018e54584654d268e030616c14193fd208800499
VladGershun <VladYGershun@Yahoo.com>
2021-12-26 23:05:39 -0800

    Created project 5

---
+++ Project5/WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Vlad Gershun on 12/26/21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
