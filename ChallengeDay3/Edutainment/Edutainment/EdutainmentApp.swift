581ca5c578400584f63a3586098eb5a9e5450fb1
VladGershun <VladYGershun@Yahoo.com>
2022-01-03 18:59:29 -0800

    Started challenge project 3.

---
+++ ChallengeDay3/Edutainment/Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Vlad Gershun on 1/3/22.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
