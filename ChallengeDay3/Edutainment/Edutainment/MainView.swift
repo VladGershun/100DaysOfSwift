e8b60f2adf311cc178e08ddc789b737504a1ab24
VladGershun <VladYGershun@Yahoo.com>
2022-01-04 17:52:29 -0800

    Working on answer array.

--- ChallengeDay3/Edutainment/Edutainment/MainView.swift
+++ ChallengeDay3/Edutainment/Edutainment/MainView.swift
@@ -8,8 +8,8 @@
 import SwiftUI
 
 struct Question {
-    var firstValue = 1
-    var secondValue = 1
+    var firstValue = 10
+    var secondValue = 4
     var question: String {
         "What Is \(firstValue) X \(secondValue)"
     }
@@ -18,28 +18,65 @@ struct Question {
     }
 }
 
-
 struct MainView: View {
     @State private var question = Question()
+    @State private var userAnswer = 0
+    @State private var answerArray = [Int]()
     
-    var body: some View {
-        NavigationView {
-            Section {
-                QuestionView(question: question)
-                    .frame(maxWidth: .infinity, maxHeight: 200)
-                    .background(.white)
-                    .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
-                    .padding()
+    func makeArray() {
+        answerArray.append(question.answer)
+        for _ in 0..<4 {
+            if question.answer < 15 {
+                answerArray.append(-Int.random(in: (question.answer - 10)...(question.answer - 4))).shuffled()
+            } else {
+                answerArray.append(Int.random(in: (question.answer - 10)...(question.answer - 4))).shuffled()
             }
-            .navigationTitle("Multiplication Is Fun!")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(
-                LinearGradient(colors: [.blue, .green], startPoint: .top, endPoint: .bottom)
-                    .ignoresSafeArea()
-            )
+           
+            
         }
+        print(answerArray)
     }
     
+    var body: some View {
+        VStack {
+            Text("Multiplication Is Fun!")
+                .font(.title)
+                .foregroundColor(.white)
+                .padding()
+            
+            Spacer()
+            
+            QuestionView(question: question)
+                .frame(maxWidth: .infinity, maxHeight: 200)
+                .background(.white)
+                .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
+                .padding()
+                .task {
+                    makeArray()
+                }
+            
+            HStack {
+                ForEach(answerArray, id: \.self) { i in
+                    Spacer()
+                    Button("\(i)") {
+                        //no-op
+                    }
+                    .buttonStyle(.borderedProminent)
+                    Spacer()
+                }
+            }
+            .padding()
+            
+            
+            Spacer()
+            Spacer()
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(
+            LinearGradient(colors: [.blue, .green], startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
+        )
+    }
 }
 
 struct QuestionView: View {
@@ -77,12 +114,6 @@ struct QuestionView: View {
         
     }
 }
-
-
-
-
-
-
 
 
 
