5a11ada5a820e4b7193d5b0563ef9e85eb738c9a
VladGershun <VladYGershun@Yahoo.com>
2022-01-06 14:54:18 -0800

    Init.

---
+++ Project7/iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Vlad Gershun on 1/6/22.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
