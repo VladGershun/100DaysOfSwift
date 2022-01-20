09e8dc67af3062f3ca5b0c6411b9e5cf54080801
VladGershun <VladYGershun@Yahoo.com>
2022-01-19 18:10:49 -0800

    Day 43 completed.

---
+++ Project9/Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Vlad Gershun on 1/19/22.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 Course Link: www.hackingwithswift.com/100/swiftui
 
-Current Day Completed: 42
+Current Day Completed: 43
 
