f888de6fd16476fd93b518acdce417380ffcdb09
VladGershun <VladYGershun@Yahoo.com>
2022-01-29 20:17:01 -0800

    Day 46 completed.

--- Project9/Drawing/Drawing/ContentView.swift
+++ Project9/Drawing/Drawing/ContentView.swift
@@ -7,33 +7,53 @@
 
 import SwiftUI
 
-struct Flower: Shape {
-    var petalOffset = -20.0
-    var petalWidth = 100.0
-    
+//struct Flower: Shape {
+//    var petalOffset = -20.0
+//    var petalWidth = 100.0
+//
+//    func path(in rect: CGRect) -> Path {
+//        var path = Path()
+//
+//        for number in stride(from: 0, to: Double.pi * 2, by: Double.pi / 8) {
+//            let rotation = CGAffineTransform(rotationAngle: number)
+//            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
+//            let originalPetal = Path(ellipseIn: CGRect(x: petalOffset, y: 0, width: petalWidth, height: rect.width / 2))
+//            let rotatedPetal = originalPetal.applying(position)
+//
+//            path.addPath(rotatedPetal)
+//        }
+//
+//        return path
+//    }
+//}
+
+struct Triangle: Shape {
     func path(in rect: CGRect) -> Path {
         var path = Path()
-         
-        for number in stride(from: 0, to: Double.pi * 2, by: Double.pi / 8) {
-            let rotation = CGAffineTransform(rotationAngle: number)
-            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
-            let originalPetal = Path(ellipseIn: CGRect(x: petalOffset, y: 0, width: petalWidth, height: rect.width / 2))
-            let rotatedPetal = originalPetal.applying(position)
-            
-            path.addPath(rotatedPetal)
-        }
+        
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
         
         return path
     }
+    
 }
 
 struct ContentView: View {
-    @State private var petalOffset = -20.0
-    @State private var petalWidth = 100.0
-    
-    @State private var amount = 0.0
+//    @State private var petalOffset = -20.0
+//    @State private var petalWidth = 100.0
+//
+//    @State private var amount = 0.0
     
     var body: some View {
+        Triangle()
+            .fill(.red)
+            .frame(width: 300, height: 300)
+    }
+        
+        
         //        VStack {
         //            Flower(petalOffset: petalOffset, petalWidth: petalWidth)
         //                .fill(.red, style: FillStyle(eoFill: true))
@@ -46,34 +66,34 @@ struct ContentView: View {
         //            Slider(value: $petalWidth, in: 0...100)
         //                .padding(.horizontal)
         //        }
-        VStack {
-            ZStack {
-                Circle()
-                    .fill(.red)
-                    .frame(width: 200 * amount)
-                    .offset(x: -50, y: -80)
-                    .blendMode(.screen)
-                
-                Circle()
-                    .fill(.green)
-                    .frame(width: 200 * amount)
-                    .offset(x: 50, y: -80)
-                    .blendMode(.screen)
-                
-                Circle()
-                    .fill(.blue)
-                    .frame(width: 200 * amount)
-                    .blendMode(.screen)
-            }
-            .frame(width: 300, height: 300)
-            
-            Slider(value: $amount)
-                .padding()
-        }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(.black)
-        .ignoresSafeArea()
-    }
+//        VStack {
+//            ZStack {
+//                Circle()
+//                    .fill(.red)
+//                    .frame(width: 200 * amount)
+//                    .offset(x: -50, y: -80)
+//                    .blendMode(.screen)
+//
+//                Circle()
+//                    .fill(.green)
+//                    .frame(width: 200 * amount)
+//                    .offset(x: 50, y: -80)
+//                    .blendMode(.screen)
+//
+//                Circle()
+//                    .fill(.blue)
+//                    .frame(width: 200 * amount)
+//                    .blendMode(.screen)
+//            }
+//            .frame(width: 300, height: 300)
+//
+//            Slider(value: $amount)
+//                .padding()
+//        }
+//        .frame(maxWidth: .infinity, maxHeight: .infinity)
+//        .background(.black)
+//        .ignoresSafeArea()
+//    }
 }
 
 
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 Course Link: www.hackingwithswift.com/100/swiftui
 
-Current Day Completed: 45
+Current Day Completed: 46
 
