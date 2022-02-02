385d0bfa73e8e82f9986c66b512fb2c11f81cbc2
VladGershun <VladYGershun@Yahoo.com>
2022-02-01 20:57:38 -0800

    Day 47 started. Habit tracker challenge app.

---
+++ ChallengeDay4/Habit/Habit/HabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitApp.swift
+//  Habit
+//
+//  Created by Vlad Gershun on 2/1/22.
+//
+
+import SwiftUI
+
+@main
+struct HabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
