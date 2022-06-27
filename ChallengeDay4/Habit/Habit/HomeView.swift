e8f2418dade6a9bc92a068990781a9e0a6ba9ab8
VladGershun <VladYGershun@Yahoo.com>
2022-06-27 11:36:24 -0600

    Update.

--- ChallengeDay4/Habit/Habit/HomeView.swift
+++ ChallengeDay4/Habit/Habit/HomeView.swift
@@ -1,40 +1,19 @@
 //
-//  ContentView.swift
+//  HomeView.swift
 //  Habit
 //
-//  Created by Vlad Gershun on 2/1/22.
+//  Created by Vlad Gershun on 6/27/22.
 //
 
 import SwiftUI
 
 struct HomeView: View {
-    @StateObject var viewModel = HabitViewModel()
-    @State private var showingAddHabit = false
-    
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(viewModel.sample) { habit in
-                    Text(habit.name)
-                }
-            }
-            .navigationTitle("Habit Tracker")
-            .navigationTitle("iExpense")
-            .toolbar {
-                Button {
-                    showingAddHabit = true
-                } label: {
-                    Image(systemName: "plus")
-                }
-            }
-            .sheet(isPresented: $showingAddHabit) {
-               AddHabitView()
-            }
-        }
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
     }
